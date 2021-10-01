4c15e1373639d878242d9f965ab7c310d882de7b
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-10-01 11:59:47 -0500

    structure added in payment and success component

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+//styles
+import '../styles/components/Payment.css';
+
 function Payment() {
   return (
-    <div>
-      <h1>Payment</h1>
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido</h3>
+        <div className="Payment-button">Boton de paypal</div>
+        <div />
+      </div>
     </div>
   );
 }
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+//styles
+import '../styles/components/Success.css';
+
 function Success() {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Franklin, Gracias por tu compra</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion:</span>
+        <div className="Success-map">Google Maps</div>
+      </div>
     </div>
   );
 }
