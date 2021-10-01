9f5da9ba53f999482ec9347a77e5fff6439ec3f5
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-10-01 10:41:13 -0500

    structure and styles added in checkout page

--- src/containers/CheckOut.jsx
+++ src/containers/CheckOut.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+//styles
+import '../styles/components/Checkout.css';
+
 function CheckOut() {
   return (
-    <div>
-      <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de compra:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item Name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio total: $10</h3>
+        <button type="button">Continuar pedido</button>
+      </div>
     </div>
   );
 }
