8aa8abec6b2dcb0d4e4031c592474d25bce56089
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-10-01 11:51:21 -0500

    added structure in info component

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
 
+//styles
+import '../styles/components/Information.css';
+
 function Information() {
   return (
-    <div>
-      <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form action="">
+            <input
+              name="name"
+              placeholder="Nombre completo"
+              type="text"
+              required
+            />
+            <input
+              name="email"
+              placeholder="Correo electronico"
+              type="email"
+              required
+            />
+            <input
+              name="address"
+              placeholder="Direccion"
+              type="text"
+              required
+            />
+            <input name="apto" placeholder="Apto" type="text" required />
+            <input name="city" placeholder="Ciudad" type="text" required />
+            <input name="country" placeholder="Pais" type="text" required />
+            <input name="state" placeholder="Estado" type="text" required />
+            <input name="cp" placeholder="Codigo Postal" type="text" required />
+            <input name="phone" placeholder="Telefono" type="text" required />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        <div className="Information-item">
+          <div className="Information-element">
+            <h4>Nombre de Item</h4>
+            <span>$10</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
