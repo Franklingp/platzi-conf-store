0a109bf60def28ddf0dfa5a0f6ab9200b8ddaf2f
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-10-26 10:02:57 -0500

    orders added in initial state, and purchase remsume added in payment component

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 
 //styles
 import '../styles/components/Payment.css';
 
 function Payment() {
+  const { state } = useContext(AppContext);
+  const { cart, buyer } = state;
+
   return (
     <div className="Payment">
       <div className="Payment-content">
         <h3>Resumen del pedido</h3>
+        {cart.map(({ title, price, id }, index) => (
+          <div className="Payment-item" key={`${id}-${index}`}>
+            <div className="Payment-element">
+              <h4>{title}</h4>
+              <span>${price}</span>
+            </div>
+          </div>
+        ))}
         <div className="Payment-button">Boton de paypal</div>
         <div />
       </div>
