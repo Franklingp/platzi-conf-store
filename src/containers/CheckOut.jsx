28c1127df4cd02cfd9e9d52e7d29d1f715e4180e
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-11-04 11:03:19 -0500

    Helmet installed and configured. Metadata and seo added in home and checkout

--- src/containers/CheckOut.jsx
+++ src/containers/CheckOut.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 //utils
 import getTotalAmount from '../utils/getTotalAmount';
@@ -20,41 +21,66 @@ function CheckOut() {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? (
-          <h3>Lista de compra:</h3>
-        ) : (
-          <div className="Checkout-empity">
-            <h3>El carrito esta vacio...</h3>
-            <i className="fas fa-wind"></i>
-            <Link to="/">Volver a la tienda</Link>
+    <>
+      <Helmet>
+        <title>Lista de pedidos - Platzi Conf Store</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@franklin_gp" />
+        <meta name="twitter:creator" content="@franklin_gp" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzistore.xyz" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        {/* <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? (
+            <h3>Lista de compra:</h3>
+          ) : (
+            <div className="Checkout-empity">
+              <h3>El carrito esta vacio...</h3>
+              <i className="fas fa-wind"></i>
+              <Link to="/">Volver a la tienda</Link>
+            </div>
+          )}
+
+          {cart.length > 0 &&
+            cart.map((product, index) => (
+              <div className="Checkout-item" key={`${product.id} - ${index}`}>
+                <div className="Checkout-element">
+                  <h4>{product.title}</h4>
+                  <span>{`$${product.price}`}</span>
+                </div>
+                <button type="button" onClick={() => removeProduct(index)}>
+                  <i className="fas fa-trash-alt"></i>
+                </button>
+              </div>
+            ))}
+        </div>
+
+        {cart.length > 0 && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio total: $${getTotalAmount(cart)}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
           </div>
         )}
-
-        {cart.length > 0 &&
-          cart.map((product, index) => (
-            <div className="Checkout-item" key={`${product.id} - ${index}`}>
-              <div className="Checkout-element">
-                <h4>{product.title}</h4>
-                <span>{`$${product.price}`}</span>
-              </div>
-              <button type="button" onClick={() => removeProduct(index)}>
-                <i className="fas fa-trash-alt"></i>
-              </button>
-            </div>
-          ))}
       </div>
-
-      {cart.length > 0 && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio total: $${getTotalAmount(cart)}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 //copmonents
 import Products from '../components/Products';
 
 function Home() {
   return (
-    <div className="Home">
-      <Products />
-    </div>
+    <>
+      {/* Metadatos y seo */}
+      <Helmet>
+        <title>Platzi Conf Store - Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@franklin_gp" />
+        <meta name="twitter:creator" content="@franklin_gp" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzistore.xyz" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        {/* <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
+      </Helmet>
+      <div className="Home">
+        <Products />
+      </div>
+    </>
   );
 }
 
